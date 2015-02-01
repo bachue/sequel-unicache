e81eaa576a168fb5ae48a1cc1cb2fd60837616b0
Bachue Zhou <bachue.shu@gmail.com>
2015-02-01 12:05:10 +0800

    Now we can use any single unicache key to read through cache

--- lib/sequel/unicache/finder.rb
+++ lib/sequel/unicache/finder.rb
@@ -13,19 +13,34 @@ module Sequel
           else
             config = unicache_for primary_key # primary key is always unicache keys, no needs to fuzzy search
             if unicache_enabled_for? config
-              key = config.key.({primary_key => pk}, config)
-              cache = config.cache.get key
-              if cache
-                values = config.deserialize.(cache, config)
-                dataset.row_proc.call values
-              else
-                model = super # cache not found
-                Write.write model if model
-                model
-              end
+              find_with_cache({primary_key => pk}, config) { super }
             else
               super
             end
+          end
+        end
+
+        def first_where hash
+          config = unicache_for(*hash.keys, fuzzy: true) # need fuzzy search, this function always returns enabled config
+          if config
+            find_with_cache(hash, config) { super }
+          else
+            super
+          end
+        end
+
+      private
+
+        # Find from cache first, if failed, get fallback result from block
+        def find_with_cache hash, config
+          cache = config.cache.get config.key.(hash, config)
+          if cache
+            values = config.deserialize.(cache, config)
+            dataset.row_proc.call values
+          else
+            model = yield
+            Write.write model if model
+            model
           end
         end
       end
